6c9b9a4df6e69022b04e1f2bc7bbf483da3c14d9
Chase Johnson <charles-e-johnson@uiowa.edu>
2019-02-18 21:46:59 -0600

    Modified sisc.v so that the modules are declared.

--- sisc.v
+++ sisc.v
@@ -9,11 +9,18 @@ module sisc (clk, rst_f, ir);
   input [31:0] ir;
 
 // declare all internal wires here
-
+wire[31:0] rsa;
+wire[31:0] rsb;
+wire[
 
 
 // component instantiation goes here
-
+module statreg (clk, in, enable, out);
+module alu (clk, rsa, rsb, imm, alu_op, alu_result, stat, stat_en);
+module mux32 (in_a, in_b, sel, out);
+module rf (clk, read_rega, read_regb, write_reg, write_data, rf_we, rsa, rsb);
+module ctrl (clk, rst_f, opcode, mm, stat, rf_we, alu_op, wb_sel);
+module mux4 (in_a, in_b, sel, out);
 
   initial
   
