572eae693b1d985b7c9dea8e0701b6c89366ce46
Chase Johnson <charles-e-johnson@uiowa.edu>
2019-02-18 22:21:54 -0600

    Added wires so the modules know where things are.

--- sisc.v
+++ sisc.v
@@ -11,16 +11,29 @@ module sisc (clk, rst_f, ir);
 // declare all internal wires here
 wire[31:0] rsa;
 wire[31:0] rsb;
-wire[
+wire[3:0] stat;
+wire enable;
+wire out;
+wire[31:0] alu_result;
+wire[1:0] alu_op;
+wire[31:0] instruction
+wire zero;
+wire wb_sel;
+wire[31:0] write_data;
+wire[3:0] read_regb;
+wire rf_we;
+wire rst_f;
+wire[1:0] alu_op;
+wire sel;
 
 
 // component instantiation goes here
-module statreg (clk, in, enable, out);
-module alu (clk, rsa, rsb, imm, alu_op, alu_result, stat, stat_en);
-module mux32 (in_a, in_b, sel, out);
-module rf (clk, read_rega, read_regb, write_reg, write_data, rf_we, rsa, rsb);
+module statreg (clk, stat, stat_en, out);
+module alu (clk, rsa, rsb, instruction[15:0], alu_op, alu_result, stat, stat_en);
+module mux32 (zero, alu_result, wb_sel, write_data);
+module rf (clk, instruction[19:16], read_regb, instruction[23:20], write_data, rf_we, rsa, rsb);
 module ctrl (clk, rst_f, opcode, mm, stat, rf_we, alu_op, wb_sel);
-module mux4 (in_a, in_b, sel, out);
+module mux4 (instruction[23:20], instruction[15,12], sel, read_regb);
 
   initial
   
