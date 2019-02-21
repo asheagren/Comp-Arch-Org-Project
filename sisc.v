88f7ff581cfa3429d83aadeeed3ab608a85639e8
Price <rjprice@uiowa.edu>
2019-02-20 19:04:51 -0600

    merge

--- sisc.v
+++ sisc.v
@@ -7,7 +7,12 @@ module sisc (clk, rst_f, ir);
 
   input clk, rst_f;
   input [31:0] ir;
-
+  input [31:0] instruction;
+  output [15:0] alu_instruction;
+  output [3:0] rf_instruction1;
+  output [3:0] rf_mux4_instruction;
+  output [3:0] mux4_instruction2;
+  output[3:0] read_regb;
 // declare all internal wires here
 wire[31:0] rsa;
 wire[31:0] rsb;
@@ -16,27 +21,32 @@ wire enable;
 wire out;
 wire[31:0] alu_result;
 wire[1:0] alu_op;
-output[31:0] instruction;
+
 wire zero;
 wire wb_sel;
 wire[31:0] write_data;
-output[3:0] read_regb;
+
 wire rf_we;
 wire rst_f;
 
-output sel;
+
 wire stat_en;
 
 
 // component instantiation goes here
 statreg statreg1(clk, stat, stat_en, out);
- alu alu1(clk, rsa, rsb, instruction[15:0], alu_op, alu_result, stat, stat_en);
+ alu alu1(clk, rsa, rsb, alu_instruction, alu_op, alu_result, stat, stat_en);
  mux32 mux321(zero, alu_result, wb_sel, write_data);
- rf rf1(clk, instruction[19:16], read_regb, instruction[23:20], write_data, rf_we, rsa, rsb);
+ rf rf1(clk, rf_instruction1, read_regb, rf_mux4_instruction, write_data, rf_we, rsa, rsb);
  ctrl ctrl1(clk, rst_f, opcode, mm, stat, rf_we, alu_op, wb_sel);
- mux4 mux41(instruction[23:20], instruction[15:12], sel, read_regb);
+ mux4 mux41(rf_mux4_instruction, mux4_instruction, sel, read_regb);
 
 initial
+  rf_instruction1 = instruction[19:16];
+	alu_instruction = instruction[15:0];
+	
+	rf_mux4_instruction = instruction[23:20];
+	mux4_instruction2 = instruction[15:12];
 	// put a $monitor statement here.  
 	$monitor($time,,"RSA=%b, RSB=%b, stat=%b, enable=%b, out=%b, alu_re=%b, alu_op=%b, instruct=%b, zero=%b, wb_sel=%b, write_data=%b, read_regb=%b, rf_we=%b, rst_f=%b, alu_op=%b, sel=%b  ", rsa,rsb,stat,enable,out,alu_result,alu_op,instruction,zero,wb_sel,write_data,read_regb,rf_we,rst_f,alu_op,sel);
 endmodule
